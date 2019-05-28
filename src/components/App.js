29f0dfa4d8d39c8c0fb8d5e5c364b6b18db571fd
devansien <devansien@gmail.com>
2019-05-28 20:46:31 +1200

    widget added

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Widget from './Widget';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Widget />
+      </div>
+    );
+  }
+}
+
+export default App;
